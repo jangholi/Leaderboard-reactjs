24743f6677b5f55b522ff5620bae06e5a8963705
marjan <marjan.jangholi@yahoo.com>
2021-02-28 12:55:27 +0330

    change english to persian number function

--- src/helpers/englishToPersianNum.js
+++ src/helpers/englishToPersianNum.js
@@ -1,48 +1,19 @@
 function englishToPersianNum(num) {
 
-    function _changeNumChar(number) {
-        let persianNumber;
-        switch (number) {
-            case '0':
-                persianNumber = '۰';
-                break;
-            case '1':
-                persianNumber = '۱';
-                break;
-            case '2':
-                persianNumber = '۲';
-                break;
-            case '3':
-                persianNumber = '۳';
-                break;
-            case '4':
-                persianNumber = '۴';
-                break;
-            case '5':
-                persianNumber = '۵';
-                break;
-            case '6':
-                persianNumber = '۶';
-                break;
-            case '7':
-                persianNumber = '۷';
-                break;
-            case '8':
-                persianNumber = '۸';
-                break;
-            case '9':
-                persianNumber = '۹';
-                break;
+    function _replaceChar(search, replace, subject) {
+        let length = search.length;
+        subject = subject.toString();
+
+        for (let i = 0; i < length; i++) {
+            subject = subject.split(search[i]).join(replace[i]);
         }
-        return persianNumber
-    }
 
-    var newNum = '';
-    for(var char of num.toString()){
-        newNum += _changeNumChar(char)
+        return subject;
     }
+    let enums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    let pnums = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
 
-    return newNum
+    return _replaceChar(enums, pnums, num);
 }
 
 export default englishToPersianNum
